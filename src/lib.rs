3acf70d41162622d5214c2e4a3031b510b6dab26
Chris Rice <patientstreetlight@gmail.com>
2023-05-25 17:03:44 -0400

    Store values in memory

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,25 @@
-pub struct KvStore;
+use std::collections::HashMap;
+
+pub struct KvStore {
+    store: HashMap<String, String>,
+}
 
 impl KvStore {
     pub fn new() -> KvStore {
-        KvStore
+        KvStore {
+            store: HashMap::new()
+        }
     }
 
     pub fn get(&self, s: String) -> Option<String> {
-        None
+        self.store.get(&s).cloned()
     }
 
     pub fn remove(&mut self, s: String) {
+        self.store.remove(&s);
     }
 
     pub fn set(&mut self, k: String, v: String) {
+        self.store.insert(k, v);
     }
 }

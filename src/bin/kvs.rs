597ced19d26a837a0f56a1ba07c6c8ea9330977f
Chris Rice <patientstreetlight@gmail.com>
2023-05-25 16:53:45 -0400

    Add basic CLI parsing

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,45 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Sets a value
+    Set {
+        key: String,
+        value: String,
+    },
+
+    /// Gets a value
+    Get {
+        key: String,
+    },
+
+    /// Removes a value
+    Rm {
+        key: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    match &cli.command {
+        Commands::Set { .. } => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+        Commands::Get { .. } => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+        Commands::Rm { .. } => {
+            eprintln!("unimplemented");
+            std::process::exit(1);
+        }
+    }
 }
